d609f1a69132f12746edc423a933eb59c5f66ee3
josh hellman and nick gallo <nick@>
2020-01-16 16:44:25 -0800

    political survery work in progress

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,57 @@
+
 $(document).ready(function() {
+
+  var q1;
+
+  $("#firstq input").change(function() {
+    console.log(this.value)
+    q1 = this.value;
+  })
+  
+
+
   $("form#political").submit (function(event) {
+
+    event.preventDefault();
     var left = 0
     var right = 0
-    if (trump === 'right'){
+    if ($("input[name='trump']:checked").val() === 'right'){
       right += 1
     }
     else {
       left += 1
     }
-    if (capitalism === 'right'){
+    console.log(("input[name='trump']:checked").val())
+    if ($(("#capitalism").value === 'right')){
       right += 1
     }
     else {
       left += 1
     }
-    if (Welfare === 'right'){
+    if ($(("#Welfare").value === 'right')){
       right += 1
     }
     else {
       left += 1
     }
-    if (UBI === 'right'){
+    if ($(("#UBI").value === 'right')){
       right += 1
     }
     else {
       left += 1
     }
-    if (babies === 'right'){
+    if ($(("#babies").value === 'right')){
       right += 1
     }
     else {
       left += 1
     }
+    console.log(right, left)
     if (right > left) {
       $(".right").show();  
     }
     else {
       $(".left").show();
     }
-    event.preventDefault();
   })
 })
